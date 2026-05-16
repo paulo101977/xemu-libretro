4c3346f38c01e908cacb9c01f7fac63f56910d99
paulo10.1977@yahoo.com.br <paulo10.1977@yahoo.com.br>
2026-05-16 16:55:39 -0300

    Fixing build on linux

--- config.sh
+++ config.sh
@@ -1,32 +1,88 @@
 #!/usr/bin/env bash
+#cd Xbox
+
+#rm -rf build
+
+#export CFLAGS="-Wno-error=old-style-definition"
+#export CXXFLAGS="-Wno-error=old-style-definition"
+#export LDFLAGS="-lcurl $LDFLAGS"
+#export LIBS="-lcurl $LIBS"
+#export MESON=/mingw64/bin/meson
+#export NINJA=/mingw64/bin/ninja
+#export PKG_CONFIG=x86_64-w64-mingw32-pkg-config
+#export PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig
+
+#./build.sh -p win64-cross
+
+#cd build
+# ninja xemu-libretro.dll
+#if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+    # Windows/MinGW
+    #ninja xemu_module.pyd
+    #cp xemu_module.pyd ../../xemu_module.pyd
+#elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    #ninja xemu_module.so
+    #cp xemu_module.so ../../xemu_module.so
+#else
+    # Fallback
+    #ninja xemu_module
+#fi
+
+#cd ../..
+
+#!/usr/bin/env bash
 cd Xbox
 
 rm -rf build
 
+platform="${OSTYPE}"
+
+echo "Detected platform: $platform"
+
+# defaults
+unset PKG_CONFIG
+unset PKG_CONFIG_PATH
+unset MESON
+unset NINJA
+
 export CFLAGS="-Wno-error=old-style-definition"
 export CXXFLAGS="-Wno-error=old-style-definition"
+
 export LDFLAGS="-lcurl $LDFLAGS"
 export LIBS="-lcurl $LIBS"
-export MESON=/mingw64/bin/meson
-export NINJA=/mingw64/bin/ninja
-export PKG_CONFIG=x86_64-w64-mingw32-pkg-config
-export PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig
 
-./build.sh -p win64-cross
+if [[ "$platform" == "linux-gnu"* ]]; then
+    echo ">>> Linux native build"
+
+    export PKG_CONFIG="pkg-config"
+
+    ./build.sh
+
+elif [[ "$platform" == "msys" || "$platform" == "cygwin" ]]; then
+    echo ">>> Windows MSYS2 build"
+
+    export PKG_CONFIG=x86_64-w64-mingw32-pkg-config
+    export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig
+    export MESON=/mingw64/bin/meson
+    export NINJA=/mingw64/bin/ninja
+
+    ./build.sh -p win64-cross
+
+else
+    echo "Unsupported platform: $platform"
+    exit 1
+fi
 
 cd build
-# ninja xemu-libretro.dll
-if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
-    # Windows/MinGW
-    ninja xemu_module.pyd
-    cp xemu_module.pyd ../../xemu_module.pyd
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux
+
+# build outputs
+if [[ "$platform" == "linux-gnu"* ]]; then
     ninja xemu_module.so
     cp xemu_module.so ../../xemu_module.so
 else
-    # Fallback
-    ninja xemu_module
+    ninja xemu_module.pyd
+    cp xemu_module.pyd ../../xemu_module.pyd
 fi
 
 cd ../..
